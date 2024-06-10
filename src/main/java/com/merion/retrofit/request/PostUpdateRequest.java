e3d45529d335ce65f5eb64b005c42c8a7f4c4e20
Sadalexa <sadalexa@gmail.com>
2024-06-10 14:28:12 +0400

    Рефлизовали запросы к jsonplaceholder API

---
+++ src/main/java/com/merion/retrofit/request/PostUpdateRequest.java
@@ -0,0 +1,40 @@
+package com.merion.retrofit.request;
+
+public class PostUpdateRequest {
+    private Integer id;
+    private Integer UserId;
+    private String title;
+    private String body;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(Integer userId) {
+        UserId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
